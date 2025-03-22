3578102ab96a82995f44b50c7c62b53ac59b7086
shreezn <asktelugu@gmail.com>
2025-03-22 18:01:30 +0530

    set up temp static pages and boilerplate fileserver

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/srijan-rahgavula/projectX
+
+go 1.23.6
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = "8888"
+
+func main() {
+	fmt.Println("Starting the main server")
+
+	staticFS := http.FileServer(http.Dir("./src/static"))
+	mux := http.NewServeMux()
+
+	mux.Handle("/v1/static/", http.StripPrefix("/v1/static", staticFS))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		staticFS.ServeHTTP(w, r)
+	})
+
+	srv := http.Server{
+		Addr:    "localhost:" + port,
+		Handler: mux,
+	}
+	fmt.Println("Listening and Serving on port: ", srv.Addr)
+	log.Fatal(srv.ListenAndServe())
+}
